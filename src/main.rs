a60d7ec83ee67a3481507cc24acd4e8ef895db15
reujab <reujab@gmail.com>
2018-07-03 21:04:54 -0400

    read zip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate zip;
+
+use std::env;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().skip(1);
+    for path in args {
+        println!("{}:", path);
+        process(path);
+    }
+}
+
+fn process(path: String) {
+    println!("Reading ZIP...");
+    let file = File::open(path).unwrap();
+    let mut zip = zip::ZipArchive::new(file).unwrap();
+
+    println!("Reading container...");
+    let container = zip.by_name("META-INF/container.xml").unwrap();
 }
