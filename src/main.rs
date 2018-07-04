fe59ce9f31b0d9b12e0aa7fd3160ee06be442e7f
reujab <reujab@gmail.com>
2018-07-03 22:06:50 -0400

    extract zip to temporary directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+extern crate tempfile;
 extern crate zip;
 
 use std::env;
 use std::fs::File;
+use std::fs;
+use std::io;
 
 fn main() {
     let args = env::args().skip(1);
@@ -16,6 +19,19 @@ fn process(path: String) {
     let file = File::open(path).unwrap();
     let mut zip = zip::ZipArchive::new(file).unwrap();
 
-    println!("Reading container...");
-    let container = zip.by_name("META-INF/container.xml").unwrap();
+    println!("Extracting ZIP...");
+    let tmp = tempfile::tempdir().unwrap();
+    for i in 0..zip.len() {
+        let mut input = zip.by_index(i).unwrap();
+        let input_path = input.sanitized_name();
+        if input_path.ends_with("/") {
+            continue;
+        }
+
+        let output_path = tmp.path().join(input_path);
+        fs::create_dir_all(output_path.parent().unwrap()).unwrap();
+        let mut output = File::create(output_path).unwrap();
+
+        io::copy(&mut input, &mut output).unwrap();
+    }
 }
