6f994e1c483a8728b6b72a37e81b20a24d58a7f5
reujab <reujab@gmail.com>
2018-07-04 00:10:28 -0400

    optimize files in epub

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 extern crate tempfile;
+extern crate walkdir;
 extern crate zip;
 
 use std::env;
 use std::fs::File;
 use std::fs;
 use std::io;
+use std::process::Command;
+use walkdir::WalkDir;
 
 fn main() {
+    let mut bytes_saved = 0;
     let args = env::args().skip(1);
     for path in args {
         println!("{}:", path);
-        process(path);
+        bytes_saved += process(path);
     }
+    println!("{}KiB saved in total.", bytes_saved / 1024);
 }
 
-fn process(path: String) {
+fn process(path: String) -> u64 {
     println!("Reading ZIP...");
     let file = File::open(path).unwrap();
     let mut zip = zip::ZipArchive::new(file).unwrap();
@@ -34,4 +39,54 @@ fn process(path: String) {
 
         io::copy(&mut input, &mut output).unwrap();
     }
+
+    println!("Minifying files...");
+    let mut bytes_saved = 0;
+    for entry in WalkDir::new(&tmp) {
+        let entry = entry.unwrap();
+        if entry.file_type().is_dir() {
+            continue;
+        }
+        let path = entry.path();
+
+        let ext = path.extension();
+        if ext == None {
+            continue;
+        }
+        let ext = ext.unwrap();
+
+        let original_len = entry.metadata().unwrap().len();
+        match ext.to_str().unwrap() {
+            "html" | "htm" | "css" | "svg" | "xml" => {
+                Command::new("minify").
+                    arg(path).
+                    arg("-o").
+                    arg(path).
+                    output().
+                    unwrap();
+            }
+            "jpeg" | "jpg" => {
+                Command::new("jpegoptim").
+                    arg("-s").
+                    arg(path).
+                    output().
+                    unwrap();
+            }
+            "png" => {
+                Command::new("crunch").
+                    arg(path).
+                    output().
+                    unwrap();
+                // FIXME when crunch adds an option to overwrite file
+                // https://github.com/chrissimpkins/Crunch/issues/20
+                fs::rename(path.parent().unwrap().join(path.file_stem().unwrap().to_str().unwrap().to_owned() + "-crunch.png"), path).unwrap();
+            }
+            _ => {}
+        }
+        bytes_saved += original_len - entry.metadata().unwrap().len();
+    }
+    println!("{}KiB saved.", bytes_saved / 1024);
+
+    println!();
+    bytes_saved
 }
